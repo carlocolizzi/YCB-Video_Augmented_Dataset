80822238d7031775a831c1205b109ea49a2d1da3
Carlo Colizzi <Ccolizzi@stlouisschoolapps.com>
2019-08-27 14:19:12 +0200

    File now successfully rototranslates pointclouds for all objects in scene

--- pointcloud-to-superquadric.py
+++ pointcloud-to-superquadric.py
@@ -3,115 +3,132 @@
 
 #This file requires python3, numpy, scipy, superquadric-lib
 
-import sys
+import sys                      #needs to be UNIX-based
 import numpy
 import os
 import scipy.io as sio
+import glob
+#import ipdb; ipdb.set_trace()                          #to trace algorithm and debugging
 
-path = input("Drag Folder Here:")
 
-#understand the number of objects present in the scene
-lines = sum(1 for line in open(path + '/000001-box.txt'))
-objects = []
-for line in open(path + '/000001-box.txt'):
-    objects.append(line.split(None, 1)[0])              #stores names of objects
+#path = input("Drag Folder Here:")
+path = "/Users/Carlo/Desktop/IIT/YCB_Video_Dataset/data/0000"
+frames = []                                                             ##
+for name in sorted(glob.glob(path + "/??????-box.txt")):                ##Stores name of each frame (Ex. 000001)
+    frames.append(name[-14:-8])                                         ##
 
-print(objects)          #DEBUG
+#pointPath = input('Drag YCB models folder here: ')
+pointPath = "/Users/Carlo/Desktop/IIT/models"
 
-#close(path + '/000001-box.txt')         #close connection to object file ??
+progressCounter = 0
 
-#store contents of mat file
-matpath = path + "/000001-meta.mat"
-mat_contents = sio.loadmat(matpath)
+for filename in frames:                           #repeat the whole process for each
 
-#store the point clouds for each object in the scene
-pointPath = input('Drag YCB models folder here: ')
-contents = [""] * lines
-coordinates = [""] * lines
+    #understand the number of objects present in the scene
+    numOfObjects = sum(1 for line in open(path + '/'+ filename + '-box.txt'))       #stores the number of objects in the scene
+    objects = []                                                                    #stores the names of the objects
+    for line in open(path + '/'+ filename + '-box.txt'):
+        objects.append(line.split(None, 1)[0])                                      #appends names of objects to list
+    #print(objects)          #DEBUG
 
-for index in range(0, lines):
-    pointClouds = open(pointPath + "/" + objects[index] + "/points.xyz", "rt")   ###CONTINUE HERE
-    contents[index] = pointClouds.read()                            #create a variable that holds the text
+    #store contents of mat file
+    matpath = path + '/'+ filename + '-meta.mat'
+    mat_contents = sio.loadmat(matpath)
 
-    #the following wouldn't be needed if python were able to identify lines in this type of file
-    coordinates[index] = [""] * contents[index].count("\n")               #initialize array that will hold coordinates with correct length
-    countertwo = 0                                                        #counts the number of different points there are
-    for counter in range(0, len(contents[index])):
-        #print(contents[index][counter])       #DEBUG
-        if contents[index][counter] != '\n':                       #separate
-            coordinates[index][countertwo] = coordinates[index][countertwo] + contents[index][counter]
-        else:
-            countertwo += 1
+    #store the point clouds for each object in the scene
+    contents = [""] * numOfObjects
+    coordinates = [""] * numOfObjects
 
-    #print(coordinates[index])                  #print array            #DEBUG
-    pointClouds.close()                  #close connection with file
+    for index in range(0, numOfObjects):                    #for each object
+        #Load Point Cloud and read its contents
+        pointClouds = open(pointPath + "/" + objects[index] + "/points.xyz", "rt")
+        contents[index] = pointClouds.read()                            #create a variable that holds the text
+        #print("OLD COORDINATES" + str(index))      #DEBUG
+        #print(contents[index])                     #DEBUG
 
-    #############################################################################
-    #########################   TESTED UNTIL HERE   #############################       #correct
-    #############################################################################
-
-    #Multiply matrices, specifically from homogenous coordinates of a point cloud with a roto-translation matrix
-
-    #Convert regular coordinates into homogenous coordinates
-    homogenousCoordinates = [""] * lines
-    for indextwo in range(0, len(coordinates[index])):
-        countertwo = 0
-        temporaryCoordinates = [""] * lines
-        temporaryCoordinates[index] = [""] * 3
-        for counter in range(0, len(coordinates[index][indextwo])):
-            if coordinates[index][indextwo][counter] != " ":
-                temporaryCoordinates[index][countertwo] = temporaryCoordinates[index][countertwo] + coordinates[index][indextwo][counter]
+        #the following wouldn't be needed if python were able to identify lines in this type of file
+        coordinates[index] = [""] * contents[index].count("\n")               #initialize array that will hold coordinates with correct length
+        countertwo = 0                                                        #counts the number of different points there are
+        for counter in range(0, len(contents[index])):
+            #print(contents[index][counter])       #DEBUG
+            if contents[index][counter] != '\n':                       #separate
+                coordinates[index][countertwo] = coordinates[index][countertwo] + contents[index][counter]
             else:
                 countertwo += 1
+        #print(coordinates[index])                  #print array            #DEBUG
+        pointClouds.close()                  #close connection with file
 
-        x = temporaryCoordinates[index][0]
-        y = temporaryCoordinates[index][1]
-        z = temporaryCoordinates[index][2]
+        #Convert regular coordinates into homogenous coordinates
+        homogenousCoordinates = [""] * numOfObjects
         homogenousCoordinates[index] = []
-        homogenousCoordinates[index].append(numpy.array([[x], [y], [z], [1]]))
 
-    print(*homogenousCoordinates, sep=', \n')       #DEBUG
+        for indextwo in range(0, len(coordinates[index])):      #repeats itself ~2600 times (each point cloud has 2621 points)
+            countertwo = 0
+            temporaryCoordinates = [""] * numOfObjects
+            temporaryCoordinates[index] = [""] * 3
+            for counter in range(0, len(coordinates[index][indextwo])):         #repeats itself 25 times (number of characters in the coordinates of a point)
+                #print(coordinates[index][indextwo][counter])
+                if coordinates[index][indextwo][counter] != " ":
+                    temporaryCoordinates[index][countertwo] = temporaryCoordinates[index][countertwo] + coordinates[index][indextwo][counter]
+                else:
+                    countertwo += 1
 
-    ################################################################
-    ########TESTED UNTIL ABOVE BUT SHOULD WORK UNTIL HERE###########
+            x = temporaryCoordinates[index][0]
+            y = temporaryCoordinates[index][1]
+            z = temporaryCoordinates[index][2]
+            homogenousCoordinates[index].append(numpy.array([[x], [y], [z], [1]]))
+        #print(homogenousCoordinates[index])
+        #print(*homogenousCoordinates, sep=', \n')       #DEBUG
+        #print(*homogenousCoordinates)
 
-    positionone = mat_contents['poses']
-    #print(positionone)                  #DEBUG
 
-    positionIdentity = [[1, 0, 0, 0],           #to verify correct multiplication
-                        [0, 1, 0, 0],
-                        [0, 0, 1, 0],
-                        [0, 0, 0, 1]]
+        positionIdentity = [[1, 0, 0, 0],           #to verify correct multiplication
+                            [0, 1, 0, 0],           #not used
+                            [0, 0, 1, 0],
+                            [0, 0, 0, 1]]
 
-    for indextwo in range(0, lines):
-        position[indextwo] = numpy.array([[positionone[0][0][indextwo], positionone[0][1][indextwo], positionone[0][2][indextwo], positionone[0][3][indextwo]],
-                                       [positionone[1][0][indextwo], positionone[1][1][indextwo], positionone[1][2][indextwo], positionone[1][3][indextwo]],
-                                       [positionone[2][0][indextwo], positionone[2][1][indextwo], positionone[2][2][indextwo], positionone[2][3][indextwo]],
-                                       [          0         ,         0           ,           0         ,          1         ]])
 
-    #print(position)             #DEBUG
-    #########
+        positionone = mat_contents.get('poses')
+        #print(positionone)            #DEBUG
 
-    #Convert new coordinates into type of file (.XYZ?)  .obf   #code actually can also use .txt file
-        #Create .txt file
-    finalPath = pointPath[:-3]+"xyz"
-    os.system("touch " + finalPath)
-    finalFile = open(finalPath, "w")
+        position = [""] * numOfObjects   #THIS IS WRONG
+        for indextwo in range(0, numOfObjects):
+            position[indextwo] = numpy.array([[positionone[0][0][indextwo], positionone[0][1][indextwo], positionone[0][2][indextwo], positionone[0][3][indextwo]],
+                                             [positionone[1][0][indextwo], positionone[1][1][indextwo], positionone[1][2][indextwo], positionone[1][3][indextwo]],
+                                             [positionone[2][0][indextwo], positionone[2][1][indextwo], positionone[2][2][indextwo], positionone[2][3][indextwo]],
+                                             [           0              ,              0              ,                0           ,                 1         ]])
 
-    #Multiply HC By roto-translation matrix to obtain new coordinates
-    position = numpy.array(position, dtype=float)
-    homogenousCoordinates = numpy.array(homogenousCoordinates, dtype=float)
-    translatedCoordinates = [""] * len(homogenousCoordinates)
-    for index in range(0, len(homogenousCoordinates)):
-        translatedCoordinates[index] = numpy.dot(position, homogenousCoordinates[index])
+        #print(position)             #DEBUG
+        #print(positionone)
 
-        #write to file
-        finalFile.write((str(*translatedCoordinates[index][0])+" "))
-        finalFile.write((str(*translatedCoordinates[index][1])+" "))
-        finalFile.write((str(*translatedCoordinates[index][2])+"\n"))
-        #finalFile.write((str(*translatedCoordinates[index][3])+"\n"))  #this is to reprint the coordinates in homogenous format
+        #Convert new coordinates into type of file (.XYZ?)  .obf   #code actually can also use .txt file
+        #Create .xyz file
+        finalPath = path + "/" + filename + "-" + objects[index] + "-rototranslated.xyz"        #one per frame
+        os.system("touch " + finalPath)
+        finalFile = open(finalPath, "w")
+
+
+        position = numpy.array(position, dtype=float)
+        homogenousCoordinatesTest = numpy.array(homogenousCoordinates[index], dtype=float)
+        #translatedCoordinates = [0] * numOfObjects
+        #translatedCoordinates[index] = []
+
+        #Multiply HC By roto-translation matrix to obtain new coordinates
+        for indexthree in range(0, homogenousCoordinatesTest.shape[0]):
+            translatedCoordinates= numpy.dot(position[index], homogenousCoordinatesTest[indexthree])
+            #print(*translatedCoordinates)
+            #write to file
+            finalFile.write((str(*translatedCoordinates[0])+" "))
+            finalFile.write((str(*translatedCoordinates[1])+" "))
+            finalFile.write((str(*translatedCoordinates[2])+"\n"))
+            #finalFile.write((str(*translatedCoordinates[index][3])+"\n"))  #this is to reprint the coordinates in homogenous format
+
+
+        #Show Progress
+    print( str(int((int(frames[progressCounter]) / int(frames[-1]))*100)) + "% Completed")
+    progressCounter += 1
 
     finalFile.close()
 
-#Use Superquadric Algorith to Create Superquadric from translated translatedCoordinates
-#os.system("Superquadric-Pipeline-Single " + finalPath)
+    #Use Superquadric Algorith to Create Superquadric from translated translatedCoordinates
+    #os.system("Superquadric-Pipeline-Single " + finalPath)             #this works only with icub installed
